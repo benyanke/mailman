0fce7d88e2333239c838ad5a605c7bd7c97b39ea
Ben Yanke <ben@benyanke.com>
2018-12-19 15:54:34 -0600

    Compiles again after working on config

--- config/main.go
+++ config/main.go
@@ -34,12 +34,15 @@ func (c Configuration) LoadConfig() {
 	// TODO: Add a method here to use a flag or env at runtime to specify a custom dir
 
 	// Set the name of the configuration file
-	var configName string = "config"
+	var configFile string = "config.yaml"
 
-	viper.SetConfigType("yaml")
+	// Set config type
+	// viper.SetConfigType(configType)
+	// viper.SetConfigName(configName) // name of config file (without extension)
 
+	// This replaces the two commands above
+	viper.SetConfigFile(configFile)
 
-	viper.SetConfigName(configName) // name of config file (without extension)
 	// System-wide config
 	viper.AddConfigPath("/etc/mailman")
 	// Look for config in the working directory
@@ -50,37 +53,23 @@ func (c Configuration) LoadConfig() {
 	// Find and read the config file
 	err := viper.ReadInConfig()
 
-
-fmt.Println(err.Error())
-
 	if err != nil {
-		if strings.HasPrefix(err.Error(), "Config File \"" + configName + "\" Not Found") {
-			fmt.Errorf("HANDLED!!!")
-			writeErr := viper.SafeWriteConfigAs(configName)
-			if writeErr != nil {
-				panic(writeErr)
-			}
+
+		// Handle specific errors before falling back to general error
+
+		// Check if error is "config not found"
+		if strings.HasPrefix(err.Error(), "Config File \"" + configFile + "\" Not Found") {
+			panic(fmt.Errorf("Configuration file could not be found. Can not continue"))
+
+			// TODO: add example0-config writing with viper.WriteConfigAs() here later
+
+		// Throw generic error
 		} else {
-	  	fmt.Println(err)
-		}
-	}
-
-	// if err.Error() == "connection lost"         // Handle errors reading the config file
-		// writeErr := v.SafeWriteConfigAs("only_if_i_dont_exist.hcl")
-	// } else
-
-
-
-
-	if err != nil {             // Handle errors reading the config file
-		if strings.HasPrefix(err.Error(), "Config File \" + configName + \" Not Found in") {
-				fmt.Errorf(err.Error())
-		} else {
-			panic(fmt.Errorf(err.Error()))
-			panic(fmt.Errorf("Fatal error config file: %s \n", err))
+			panic(err)
 		}
 
 	}
+
 
 	c.loaded = true
 
